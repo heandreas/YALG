89dacee4dceed781b5e40cc36574c78df8d10473
Andreas Henne <heandreas@live.de>
2015-09-10 12:23:57 +0200

    Yeah.

---
+++ app/src/main/java/nodomain/yalg/LaserRenderer.java
@@ -0,0 +1,18 @@
+package nodomain.yalg;
+
+import java.util.Vector;
+
+/**
+ * Created by andreas on 10.09.2015.
+ */
+public class LaserRenderer {
+    LaserRenderer(Vector<float[]> lines)
+    {
+
+    }
+
+    public void render()
+    {
+
+    }
+}
