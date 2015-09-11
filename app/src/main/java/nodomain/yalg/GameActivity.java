7109863555a004bfb1e1e56918d8e5ba883a25a2
Andreas Henne <heandreas@live.de>
2015-09-12 00:25:21 +0200

    Yeah.

--- app/src/main/java/nodomain/yalg/GameActivity.java
+++ app/src/main/java/nodomain/yalg/GameActivity.java
@@ -4,6 +4,7 @@ import nodomain.yalg.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.PointF;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +15,7 @@ import android.opengl.GLSurfaceView;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,21 +28,66 @@ public class GameActivity extends Activity {
 
     YalgGLSurface glSurface;
 
+    private List<GameObject> gameObjects = null;
+
+    public void computeLasers(List<PointF> laserSegments, List<ColorF> laserColors) {
+        if (gameObjects == null)
+            return;
+
+        ArrayList<PointF> obstacleLines = new ArrayList<PointF>();
+        ArrayList<Float> coefficients = new ArrayList<Float>();
+        ArrayList<PointF> origins = new ArrayList<PointF>();
+        ArrayList<PointF> dirs = new ArrayList<PointF>();
+        for (GameObject go : gameObjects) {
+            go.getRefractors(obstacleLines, coefficients);
+            go.getRays(origins, dirs);
+        }
+        PointF[] lineSegmentsArray = new PointF[obstacleLines.size()];
+        for (int i = 0; i < obstacleLines.size(); i++)
+            lineSegmentsArray[i] = obstacleLines.get(i);
+        float[] coefficientsArray = new float[coefficients.size()];
+        for (int i = 0; i < coefficients.size(); i++)
+            coefficientsArray[i] = coefficients.get(i);
+
+        for (int i = 0; i < origins.size(); i++)
+        {
+            LaserTracer.Result result = LaserTracer.traceRecursion(origins.get(i), dirs.get(i),
+                    1.0f, lineSegmentsArray, coefficientsArray);
+            for (int j = 0; j < result.lineSegments.size(); j++) {
+                laserSegments.add(result.lineSegments.get(j));
+                if (j % 2 == 0)
+                    laserColors.add(new ColorF(0, 1, 0));
+            }
+        }
+
+        // Hack: Add obstacles as lasers. Yeah...
+        for (int i = 0; i < obstacleLines.size(); i++) {
+            PointF p = obstacleLines.get(i);
+            laserSegments.add(p);
+            if (i % 2 == 0)
+                laserColors.add(new ColorF(0, 0, 1));
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        glSurface = new YalgGLSurface(this);
-        setContentView(glSurface);
-
         try {
-            List<GameObject> objects = LevelLoader.parse(getResources().openRawResource(R.raw.testlevel));
-            System.out.println("Read " + objects.size() + " objects.");
-            glSurface.setGameObjects(objects);
+            gameObjects = LevelLoader.parse(getResources().openRawResource(R.raw.testlevel));
+            System.out.println("Read " + gameObjects.size() + " objects.");
         } catch (XmlPullParserException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        ArrayList<PointF> laserSegments = new ArrayList<PointF>();
+        ArrayList<ColorF> laserColors = new ArrayList<ColorF>();
+        computeLasers(laserSegments, laserColors);
+
+        glSurface = new YalgGLSurface(this);
+        setContentView(glSurface);
     }
 }
--- app/src/main/java/nodomain/yalg/YalgGLSurface.java
+++ app/src/main/java/nodomain/yalg/YalgGLSurface.java
@@ -21,56 +21,23 @@ public class YalgGLSurface extends GLSurfaceView {
 
     private LaserRenderer laserRenderer = null;
 
-    private List<GameObject> gameObjects = null;
+    GameActivity gameActivity;
 
-    public void setGameObjects(List<GameObject> gameObjects) {
-        this.gameObjects = gameObjects;
-    }
 
     void renderFrame() {
-        if (gameObjects == null || laserRenderer == null)
-            return;
-
-        ArrayList<PointF> lineSegments = new ArrayList<PointF>();
-        ArrayList<Float> coefficients = new ArrayList<Float>();
-        ArrayList<PointF> origins = new ArrayList<PointF>();
-        ArrayList<PointF> dirs = new ArrayList<PointF>();
-        for (GameObject go : gameObjects) {
-            go.getRefractors(lineSegments, coefficients);
-            go.getRays(origins, dirs);
-        }
-        PointF[] lineSegmentsArray = new PointF[lineSegments.size()];
-        for (int i = 0; i < lineSegments.size(); i++)
-            lineSegmentsArray[i] = lineSegments.get(i);
-        float[] coefficientsArray = new float[coefficients.size()];
-        for (int i = 0; i < coefficients.size(); i++)
-            coefficientsArray[i] = coefficients.get(i);
-
         ArrayList<PointF> laserSegments = new ArrayList<PointF>();
         ArrayList<ColorF> laserColors = new ArrayList<ColorF>();
-        for (int i = 0; i < origins.size(); i++)
-        {
-            /*LaserTracer.Result result = LaserTracer.traceRecursion(origins.get(i), dirs.get(i),
-                    1.0f, lineSegmentsArray, coefficientsArray);
-            for (PointF p : result.lineSegments)
-                laserSegments.add(p);*/
-        }
 
-        // Hack: Add obstacles as lasers. Yeah...
-        for (int i = 0; i < lineSegments.size(); i++) {
-            PointF p = lineSegments.get(i);
-            laserSegments.add(p);
-            if (i % 2 == 0)
-                laserColors.add(new ColorF(0, 0, 1));
-
-        }
+        gameActivity.computeLasers(laserSegments, laserColors);
 
         laserRenderer.setLasers(laserSegments, laserColors);
         laserRenderer.render();
     }
 
-    public YalgGLSurface(Context context) {
-        super(context);
+    public YalgGLSurface(GameActivity gameActivity) {
+        super(gameActivity);
+
+        this.gameActivity = gameActivity;
 
         // TODO: remove debug call
         PointF[] obstacles = {new PointF(-0.4f, -0.2f), new PointF(0.4f, -0.2f),
