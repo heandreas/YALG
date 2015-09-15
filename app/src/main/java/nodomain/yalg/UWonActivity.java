7e349c6a8e4426cd192569b21d5720e988c56b8a
Andreas Henne <heandreas@live.de>
2015-09-15 16:17:37 +0200

    Yeah!

--- app/src/main/java/nodomain/yalg/UWonActivity.java
+++ app/src/main/java/nodomain/yalg/UWonActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class UWonActivity extends AppCompatActivity {
+public class UWonActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
