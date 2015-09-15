fbb53bbbc039767a1c78c71907e481f65ee625ee
kaktus3000 <1nsane@online.de>
2015-09-15 15:49:28 +0200

    added level storage

--- app/src/main/java/nodomain/yalg/UWonActivity.java
+++ app/src/main/java/nodomain/yalg/UWonActivity.java
@@ -1,6 +1,9 @@
 package nodomain.yalg;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,6 +21,19 @@ public class UWonActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         final int lastLevelID = extras.getInt("Level");
 
+        //calculate index of next level
+        int nextLevel = lastLevelID + 1;
+        if (nextLevel >= YALG.m_Levels.length)
+            nextLevel = 0;
+
+        final int nextLevelForSure = nextLevel;
+
+        //write current level to stash
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.level_file_key), Context.MODE_PRIVATE) ;
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("level", nextLevelForSure);
+        editor.commit();
+
         final Button startGameButton = (Button)findViewById(R.id.next_level_button);
         startGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,10 +41,8 @@ public class UWonActivity extends AppCompatActivity {
                 Intent myIntent = new Intent(UWonActivity.this, GameActivity.class);
 
                 //this will hold the level to load
-                int nextLevel = lastLevelID + 1;
-                if (nextLevel >= YALG.m_Levels.length)
-                    nextLevel = 0;
-                myIntent.putExtra("Level", nextLevel);
+
+                myIntent.putExtra("Level", nextLevelForSure);
                 myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
 
                 //start the actual game screen
